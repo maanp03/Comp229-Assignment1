9b3a17e7e7443377ecb83558d59da3ba5c4b37ed
Maan Patel <maanpatel1811@gmail.com>
2024-01-05 11:13:56 -0600

    updated project section

--- routes/index.js
+++ routes/index.js
@@ -20,10 +20,6 @@ router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects' });
 });
 
-/* GET services page. */
-router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services' });
-});
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
